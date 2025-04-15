f717fd67d08ea357d613bf69db7143e784b6e909
monaco11m <elmer.chp@gmail.com>
2025-04-15 02:36:05 -0500

    update CsvExtractor to return a dictionary

--- src/extractors/csv_extractor.py
+++ src/extractors/csv_extractor.py
@@ -1,27 +1,22 @@
 import os
 import pandas as pd
-from typing import List
+from typing import Dict
 from .base_extractor import BaseExtractor
 
 
 class CsvExtractor(BaseExtractor):
     
-    #constructor
-    def __init__(self, file_paths: List[str]):
-        self.file_paths = file_paths
+    def __init__(self, file_paths: Dict[str, str]):
+        self.file_paths = file_paths  # Dictionary like {"product": "E:/.../product.csv", ...}
 
+    def extract(self) -> Dict[str, pd.DataFrame]:
+        dataframes = {}
 
-    #method
-    def extract(self) -> pd.DataFrame:
-
-        dfs = []  # List to store individual DataFrames
+        for key, path in self.file_paths.items():
+            if os.path.exists(path):
+                df = pd.read_csv(path)
+                dataframes[key] = df
+            else:
+                print(f"File not found: {path}")
         
-        for file_path in self.file_paths:
-            if os.path.exists(file_path):
-                df = pd.read_csv(file_path)  # Load the CSV
-                dfs.append(df)  # Append it to the list
-        
-        # Combine all DataFrames into one if you want
-        combined_df = pd.concat(dfs, ignore_index=True)
-        
-        return combined_df
+        return dataframes

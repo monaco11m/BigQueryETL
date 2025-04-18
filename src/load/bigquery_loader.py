223c9a501aa55981ef27fbd1d7645f324994d66f
monaco11m <elmer.chp@gmail.com>
2025-04-18 01:42:17 -0500

    update BigQueryLoader and addapt for GCP

--- src/load/bigquery_loader.py
+++ src/load/bigquery_loader.py
@@ -3,13 +3,24 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 from .base_loader import BaseLoader
 import config  
+import os
 
 class BigQueryLoader(BaseLoader):
     def __init__(self):
-        credentials_bigQuery = service_account.Credentials.from_service_account_file(
-            config.GOOGLE_CREDENTIALS_PATH
-        )
-        self.client = bigquery.Client(credentials=credentials_bigQuery, project=config.BQ_PROJECT_ID)
+        # Try to use local credentials (.json) if they are defined or if the file exists
+        if (
+            hasattr(config, "GOOGLE_CREDENTIALS_PATH") and 
+            config.GOOGLE_CREDENTIALS_PATH and 
+            os.path.exists(config.GOOGLE_CREDENTIALS_PATH)
+        ):
+            credentials_bigQuery = service_account.Credentials.from_service_account_file(
+                config.GOOGLE_CREDENTIALS_PATH
+            )
+            self.client = bigquery.Client(credentials=credentials_bigQuery, project=config.BQ_PROJECT_ID)
+        else:
+            # if there is no local credential file, it means its executing by GCP then use default credentials
+            self.client = bigquery.Client(project=config.BQ_PROJECT_ID)
+
 
     def load(self, df: pd.DataFrame, table_name: str) -> None:
         job_config = bigquery.LoadJobConfig(
